ef56c8fddc709b74cf0c8ae22101412576b20262
semiprohacker <87223690+semiprohacker@users.noreply.github.com>
2024-12-08 22:15:24 +0530

    Add files via upload

---
+++ regionsafetychecker/regionsafetychecker/src/main/java/regionsafetychecker/RegionsafetycheckerApplication.java
@@ -0,0 +1,13 @@
+package regionsafetychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionsafetycheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegionsafetycheckerApplication.class, args);
+	}
+
+}
---
+++ regionsafetychecker/regionsafetychecker/src/test/java/regionsafetychecker/RegionsafetycheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package regionsafetychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionsafetycheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
